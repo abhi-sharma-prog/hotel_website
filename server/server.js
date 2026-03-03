3612503ab74ab2014a96a60d1cf6d2036c6078d3
Abhishek Sharma <abhishek280902@gmail.com>
2026-03-04 01:37:21 +0530

    render server updated

--- server/server.js
+++ server/server.js
@@ -38,11 +38,7 @@ app.get("/", (_req, res) => {
     res.sendFile(path.join(__dirname, "../client/restaurant-main/index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
-
-const server = app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+;
 
 server.on("error", (err) => {
     if (err.code === "EADDRINUSE") {
